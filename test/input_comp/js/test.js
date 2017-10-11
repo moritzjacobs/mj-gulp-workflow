db6f503e79f210766f323451b2cdb1a2a26b1c3e
Moritz Jacobs <moritz@yeah.de>
2017-10-11 10:01:02 +0200

    update tests

--- test/input_comp/js/test.js
+++ test/input_comp/js/test.js
@@ -1,16 +1,4 @@
-"use strict";
-function hi() {
-	return "hi, i am number 1";
-}
-("use strict");
-function howAreYou() {
-	return "How are you my dear?";
-}
-("use strict");
-function bye() {
-	return "Bye bye, it was nice to meet you";
-}
-("use strict");
-var foo = function foo() {
-	return "test";
-};
+"use strict";function hi(){return"hi, i am number 1"}
+"use strict";function howAreYou(){return"How are you my dear?"}
+"use strict";function bye(){return"Bye bye, it was nice to meet you"}
+"use strict";var foo=function foo(){return"test"};
