cdfc7a3acb06c839b97e2931217ea0774b0dde13
Moritz Jacobs <mail@moritzjacobs.de>
2017-10-11 22:33:14 +0200

    add pngquant to imagemin task

--- tasks/images/task.js
+++ tasks/images/task.js
@@ -1,4 +1,5 @@
 const imagemin = require("gulp-imagemin");
+const pngquant = require("imagemin-pngquant");
 
 module.exports = (gulp, config, paths) => {
 	gulp.task("images", () => {
@@ -10,6 +11,7 @@ module.exports = (gulp, config, paths) => {
 
 			if (isEnabled(config.imagemin.enabled)) {
 				buffer = buffer.pipe(imagemin(config.imagemin.config));
+				buffer = buffer.pipe(imagemin(pngquant()));
 			}
 
 			buffer.pipe(gulp.dest(dest));
