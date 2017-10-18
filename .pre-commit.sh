6dbcba7e47948032df0f8140ac8cce5aee9ae921
Moritz Jacobs <moritz@yeah.de>
2017-10-18 15:55:41 +0200

    Typo

--- .pre-commit.sh
+++ .pre-commit.sh
@@ -2,4 +2,4 @@ echo "********************************"
 echo "*  PRETTIERJS pre-commit-hook  *"
 echo "********************************"
 
-git diff — name-only HEAD | grep ".*\.js" | xargs prettier — write
+git diff -- name-only HEAD | grep ".*\.js" | xargs prettier — write
