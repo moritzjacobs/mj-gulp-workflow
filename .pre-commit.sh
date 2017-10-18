41ac5b80a351fd167ac54c33770714f76818fd70
Moritz Jacobs <moritz@yeah.de>
2017-10-18 15:54:48 +0200

    Add pre-commit-hook script for prettier

---
+++ .pre-commit.sh
@@ -0,0 +1,5 @@
+echo "********************************"
+echo "*  PRETTIERJS pre-commit-hook  *"
+echo "********************************"
+
+git diff — name-only HEAD | grep ".*\.js" | xargs prettier — write
